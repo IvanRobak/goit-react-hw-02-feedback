f1cbc5fb7f290cee9e63923ac00a56b408f79d4f
ivan1810 <78267912+ivan1810@users.noreply.github.com>
2023-04-26 15:55:51 +0300

    add button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,10 @@ class App extends Component {
     bad: 0,
   };
 
+  handleButtonClick = options => {
+    this.setState(prevState => ({ [options]: prevState[options] + 1 }));
+  };
+
   countTotalFeedback() {
     const { good, neutral, bad } = this.state;
     const totalFeedback = good + neutral + bad;
@@ -16,7 +20,7 @@ class App extends Component {
 
   countPositiveFeedbackPercentage() {
     const { good } = this.state;
-    const totalFeedback = this.countTotalFeedback;
+    const totalFeedback = this.countTotalFeedback();
     const positiveFeedbackPercentage = Math.round((good / totalFeedback) * 100);
     return positiveFeedbackPercentage;
   }
@@ -28,6 +32,12 @@ class App extends Component {
     return (
       <div>
         <h2>Please leave feedback</h2>
+        <button onClick={() => this.handleButtonClick('good')}>Good</button>
+        <button onClick={() => this.handleButtonClick('neutral')}>
+          Neutral
+        </button>
+        <button onClick={() => this.handleButtonClick('bad')}>Bad</button>
+        <h2>Statistics</h2>
         <Statistics
           good={good}
           neutral={neutral}
